0eefca8e474048e24c445459af101c1b3b2ee403
Eder Tavares <edert.tavares@gmail.com>
2023-10-14 17:30:52 -0300

    Primeiro Exemplo2

---
+++ course/src/main/java/model/entities/Invoice.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.entities;
+
+/**
+ *
+ * @author eder
+ */
+public class Invoice {
+    private Double basicPayment;
+    private Double tax;
+
+    public Invoice() {
+    }
+
+    public Invoice(Double basicPayment, Double tax) {
+        this.basicPayment = basicPayment;
+        this.tax = tax;
+    }
+
+    public Double getBasicPayment() {
+        return basicPayment;
+    }
+
+    public void setBasicPayment(Double basicPayment) {
+        this.basicPayment = basicPayment;
+    }
+
+    public Double getTax() {
+        return tax;
+    }
+
+    public void setTax(Double tax) {
+        this.tax = tax;
+    }
+    
+    public Double getTotalPayment(){
+        return getBasicPayment() + getTax();
+    }
+    
+    
+}
---
+++ course/src/main/java/model/services/RentalService.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.services;
+
+import java.time.Duration;
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+/**
+ *
+ * @author eder
+ */
+public class RentalService {
+    
+    private Double pricePerHour;
+    private Double pricePerDay;
+    
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+        this.pricePerHour = pricePerHour;
+        this.pricePerDay = pricePerDay;
+        this.taxService = taxService;
+    }
+    
+    public void processInvoice(CarRental carRental){
+        
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60.0;
+        
+        double basicPayment;
+        if(hours <= 12.0){
+            basicPayment = pricePerHour * Math.ceil(hours);
+        }
+        else{
+           basicPayment = pricePerDay * Math.ceil(hours / 24);
+        }
+        
+        double tax = taxService.tax(basicPayment);
+        
+        carRental.setInvoice(new Invoice(basicPayment , tax));
+    }
+}
